11906a09d7dc31cc7cb10004b3f331d6b357f944
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-09-14 21:24:58 -0400

    minimal cleanup WIP audio

--- src/components/Red/index.js
+++ src/components/Red/index.js
@@ -3,10 +3,20 @@ import "./red.css";
 import Button from "../Button";
 import speaker1 from "../../assets/speaker-right.png";
 import speaker2 from "../../assets/speaker-left.png";
+import music from "../../assets/Adam Port - Planet 9.mp3";
 
 const Red = () => {
+  const myAudio = document.getElementById("myAudio");
+
+  const togglePlay = () => {
+    if (myAudio !== null) {
+      return myAudio.paused ? myAudio.play() : myAudio.pause();
+    }
+  };
+
   return (
     <div className="red">
+      <audio id="myAudio" src={music} preload="auto" />
       <div className="red-container">
         <div className="button-container">
           <Button text="TRY IT NOW" location="pricing" />
@@ -19,16 +29,24 @@ const Red = () => {
             </p>
             <Button text="SEE DEMO" location="#" />
           </div>
-          <div className="red-sub-img-container">
+          <div
+            className="red-sub-img-container"
+            data-tooltip="Hey! Click on me!"
+          >
             <img
+              onClick={togglePlay}
               src={speaker1}
               alt="speaker"
               width="80%"
               style={{ width: "270px" }}
             />
           </div>
-          <div className="red-sub-img-container2">
+          <div
+            className="red-sub-img-container2"
+            data-tooltip="No! Click on me!"
+          >
             <img
+              onClick={togglePlay}
               src={speaker2}
               alt="speaker"
               width="80%"
